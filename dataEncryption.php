775782692ea4e9ea3b33c6996093c28772fa430c
JuanCarlos407 <31823343+Juancarlos407@users.noreply.github.com>
2020-01-22 11:07:49 +0100

    Update dataEncryption.php

--- dataEncryption.php
+++ dataEncryption.php
@@ -1,5 +1,24 @@
 <?php
-//Add into this file your code to solve the 15th and 16th questions of the Microtasca GS_0376 4.1
+
+//declaración de variables faltan los certificados
+$certificate=[''];
+$key=[''];
+$certificateFile=[''];
+
+//obtención de clave pública
+$publicKey= openssl_pkey_get_public( mixed $certificate ) : resource;
+//obtención de clave privada
+$privateKey= openssl_pkey_get_private( mixed $key ) : resource;
+//obtención de id de certificado
+$certificateId= openssl_x509_read( mixed $x509certdata ) : resource;
+
+//encriptación de contraseñas con sha256
+$hashPasswordEncryptionResult=password_hash("IEDIB_GS0376", CRYPT_SHA256)."</p>";
+//encriptación de archivos con sha1
+file_put_contents('proves_data_encryption.odt', 'Password = IEDIB_GS0376');
+$hashFileEncryptionResult=hash_file('SHA1', 'proves_data_encryption.odt')."</p>";
+//salidas en pantalla
 echo $hashPasswordEncryptionResult;
 echo $hashFileEncryptionResult;
+
 ?>
